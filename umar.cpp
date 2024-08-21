bb9fa61fe5b610532a62a2817a42248f4867df27
Umar-dev-01 <01.dev.umar@gmail.com>
2024-08-21 07:19:49 +0000

    first commit

--- umar.cpp
+++ umar.cpp
@@ -5,6 +5,6 @@ int main() {
    float pi=3.14;
    cout<<"this is factorial 4 \n here the valun of a is"<<a<<"\n the valu of b is"<<b;
    cout<<"\n the valu of pi is"<<pi;
-
+//augest 21 time 12 20pm
     return 0;
 }
