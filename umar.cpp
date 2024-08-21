25c5bc65c339bd8c8cdd2b12df17e6052259f00b
Umar-dev-01 <01.dev.umar@gmail.com>
2024-08-21 16:32:05 +0000

    sum

--- umar.cpp
+++ umar.cpp
@@ -2,13 +2,14 @@
 using namespace std;
 int main() 
 {
- int num=1, num=2;
+ int num1, num2;
  cout<<"enter the the value of num1:\n";
- cin>>num 1;
+ cin>>num1;
  cout<<"enter the value of num 2:\n";
 cin>>num2;
-cout<<"the sum is"<<num1+num2;
+cout<<"the sum is "<<num1+num2<<endl;
 
 
     return 0;
 }
+
