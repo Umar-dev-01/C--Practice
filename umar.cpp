f7fc1ecca05888b7be4dde15b593c60f079ff5fb
Umar-dev-01 <01.dev.umar@gmail.com>
2024-08-21 13:30:36 +0000

    first commit

--- umar.cpp
+++ umar.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 using namespace std;
 int main() {
-   int a=14, b=15;
-   float pi=3.14;
-   cout<<"this is factorial 4 \n here the valun of a is"<<a<<"\n the valu of b is"<<b;
-   cout<<"\n the valu of pi is"<<pi;
+  //nint a=14, b=15;
+ //   float pi=3.14;
+  // cout<<"this is factorial 4 \n here the valun of a is"<<a<<"\n the valu of b is"<<b;
+   //cout<<"\n the valu of pi is"<<pi;
 //augest 21 time 12 20pm
+int a=4;
+cout<<"enter integer"<<a;
     return 0;
 }
