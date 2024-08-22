7f97fe5fad590ac588e975307dd7f16b3793f9a2
Umar-dev-01 <01.dev.umar@gmail.com>
2024-08-22 01:02:26 +0000

    all simpe operator

--- umar.cpp
+++ umar.cpp
@@ -2,13 +2,24 @@
 using namespace std;
 int main() 
 {
- int num1, num2;
- cout<<"enter the the value of num1:\n";
- cin>>num1;
- cout<<"enter the value of num 2:\n";
-cin>>num2;
-cout<<"the sum is "<<num1+num2<<endl;
-
+ //int num1, num2;
+// cout<<"enter the the value of num1:\n";
+ //cin>>num1;
+// cout<<"enter the value of num 2:\n";
+//cin>>num2;
+//cout<<"the sum is "<<num1+num2<<endl;
+int a=7,b=10;
+cout<<"fallowing or the types of oprerators in c++"<<endl;
+cout<<"the value of a + b is "<<a+b<<endl;
+cout<<"the value of a - b is "<<a-b<<endl;
+cout<<"the value of a * b is "<<a*b<<endl;
+cout<<"the value of a / b is "<<a/b<<endl;
+cout<<"enter the value of a % b is "<<a%b<<endl;
+// 2 short program attached in one
+cout<<"the value of ++a "<<++a<<endl;
+cout<<"the value of a++ "<<a++<<endl;
+cout<<"the value of a-- "<<a--<<endl;
+cout<<"the valu of --a "<<--a<<endl;
 
     return 0;
 }
