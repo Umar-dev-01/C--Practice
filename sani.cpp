3500c79951d89b5a6294e686ce13c37b74aa921b
Umar.dev <01.dev.umar@gmail.com>
2024-08-28 03:48:58 +0000

    break statment

--- sani.cpp
+++ sani.cpp
@@ -4,18 +4,19 @@
 using namespace std;
 
 int main() {
-    for (int i=1; i<=40; i++)
+   /* for (int i=1; i<=40; i++)
    {
    cout<<i<<endl;  
-   }   
+   }*/   
 
     //                                                ""  
-    /*int i=0; 
+    int i=0; 
     for(int i = 0; i < 40; i++)
     {
     if(i==25){
     break;
     }
-    cout<<i<<endl;*/                                                                                      
+    cout<<i<<endl;
+    }                                                                                    
 return 0;
 }
