50726b716a126bddc3fe18ed6b0d3cdcb6cf8d93
Umar.dev <01.dev.umar@gmail.com>
2024-08-28 03:39:35 +0000

    moskel program

--- sani.cpp
+++ sani.cpp
@@ -1,3 +1,4 @@
+//                                                 "for loop"
 #include <iostream>
 #include <iomanip>
 using namespace std;
@@ -6,6 +7,15 @@ int main() {
     for (int i=1; i<=40; i++)
    {
    cout<<i<<endl;  
-   }                                                                                               
+   }   
+
+    //                                                ""  
+    /*int i=0; 
+    for(int i = 0; i < 40; i++)
+    {
+    if(i==25){
+    break;
+    }
+    cout<<i<<endl;*/                                                                                      
 return 0;
 }
