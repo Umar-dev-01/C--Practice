d2f461a64c8f8b7d1a51317856dc8ab0c64a0aea
Umar.dev <01.dev.umar@gmail.com>
2025-03-07 10:05:30 +0000

    only arranga

--- war_for.cpp
+++ war_for.cpp
@@ -24,7 +24,7 @@ cout<<sum;
 }
 
 
-Write a program to print the multiplication table of a given number using a for loop.*/
+Write a program to print the multiplication table of a given number using a for loop.
 #include<iostream>
 using namespace std;
 int main(){
@@ -38,6 +38,69 @@ cout<<a*i<<endl;
     }
 
 }
+Write a program to print all even numbers from 1 to n using a for loop.
+#include<iostream>
+using namespace std;
+int main (){
+    int a;
+    cout<<"enter yoiur num"<<endl;
+    cin>>a;
+    for (int i=2; i<=a; i+=2){
+    cout<<i<<" ";
+    }
+}
+Write a program to print all odd numbers from 1 to n using a for loop.
+#include<iostream>
+using namespace std;
+int main (){
+    int a;
+    cout<<"enter yoiur num"<<endl;
+    cin>>a;
+    for(int i=1; i<=a; i+=2){
+        cout<<i<<" ";
+    }
+}
+Write a program to print numbers from n to 1 in reverse order using a for loop.
+#include<iostream>
+using namespace std;
+int main (){
+    int a;
+    cout<<"enter yoiur num"<<endl;
+    cin>>a;
+    for (int i=1; i<=a; a--)
+    {
+      cout<<a<<" ";
+    }
+}
+
+Write a program to calculate the factorial of a given number using a for loop.
+#include<iostream>
+using namespace std;
+int main (){
+    int a, factorial=1;
+    cout<<"enter yoiur num"<<endl;
+    cin>>a;
+for (int i = 1; i <=a; i++)
+{
+    factorial*=i;
+}
+cout<<factorial;
+}
+
+Write a program to find the sum of all even numbers from 1 to n using a for loop.
+#include<iostream>
+using namespace std;
+int main (){
+int a, sum=0;
+    cout<<"enter your num"<<endl;
+    cin>>a;
+for(int i=2; i<=a; i+=2){
+    sum+=i;
+}
+cout<<sum;
+}
+Write a program to print the multiplication table of a given number in reverse order using a for loop.*
+
  
 
 
