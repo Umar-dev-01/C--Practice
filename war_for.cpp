584035a3897485e9d8f68db7a082187c73eb3e82
Umar.dev <01.dev.umar@gmail.com>
2025-03-07 09:11:43 +0000

    for first 3 important

---
+++ war_for.cpp
@@ -0,0 +1,49 @@
+/*Write a program to print numbers from 1 to n using a for loop
+#include<iostream>
+using namespace std;
+int main(){
+int a;
+cout<<"entrer the number"<<endl;
+cin>>a;
+for (int i=1; i<=a; i++)
+cout<<i<<endl;
+}
+
+Write a program to find the sum of the first n natural numbers using a for loop.
+#include<iostream>
+using namespace std;
+int main (){
+int n;
+cout<<"enter your num "<<endl;
+cin>>n;
+int sum;
+for(int i=1; i<=n; i++){
+    sum+=i;
+}
+cout<<sum;
+}
+
+
+Write a program to print the multiplication table of a given number using a for loop.*/
+#include<iostream>
+using namespace std;
+int main(){
+    int a, mult;
+    cout<<" enter your num"<<endl;
+    cin>>a;
+    for(int i=1; i<=10; i++){
+        
+       
+cout<<a*i<<endl;
+    }
+
+}
+ 
+
+
+
+
+
+
+
+
