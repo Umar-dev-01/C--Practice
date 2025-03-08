0b682826754f00148f1547480385fe1391a85e87
Umar.dev <01.dev.umar@gmail.com>
2025-03-08 16:41:09 +0000

    arry prac

---
+++ arry.cpp
@@ -0,0 +1,10 @@
+/*Write a program that declares an array of 5 integers and prints all its elements.*/
+#include<iostream>
+using namespace std;
+int main(){
+int num[]={45,52,63,85};
+cout<<num[2]<<endl;
+cout<<num[0]<<endl;
+cout<<num[1]<<endl;
+cout<<num[3]<<endl;
+}
