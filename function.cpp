d3402795ab28c1ab4f47014dc558474d2790b18c
Umar.dev <01.dev.umar@gmail.com>
2025-03-13 06:53:38 +0000

    comu

--- function.cpp
+++ function.cpp
@@ -2,7 +2,7 @@
 Write a function named addNumbers that takes two integers as arguments and returns their sum.
 📌 Example Input: addNumbers(5, 3)
 📌 Expected Output: 8
-*/
+
 #include<iostream>
 using namespace std;
 
@@ -13,3 +13,37 @@ int main() {
     int a = 3, b = 4;
     cout << addNumbers (a , b); 
 }
+Write a function named multiplyNumbers that takes two integers as arguments and returns their product.
+#include<iostream>
+using namespace std;
+
+int multiplyNumbers(int a, int b){
+    return a*b;
+}
+int main(){
+    int result=multiplyNumbers(2,8);
+    cout<<"Multiplaiy "<<result;
+}
+Write a function named divideNumbers that takes two integers as arguments and returns their quotient.*/
+#include<iostream>
+using namespace std;
+
+int divideNumbers(int a,int b){
+    return 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
