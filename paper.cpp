96fa890b22dc9b8a8bef7739f985c46559eab159
Umar.dev <01.dev.umar@gmail.com>
2025-03-16 16:59:07 +0000

    check

---
+++ paper.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+int num, i;
+cout<<"enter the number";
+cin>>num;
+while(num>0){
+    cout<<"wt enteri pao potr g";
+    cin>>num;
+}
+cout<<"na bcha g na";
+}
+
