1c46150a27342eede473af1694365e0db742a15f
Umar.dev <01.dev.umar@gmail.com>
2025-03-22 08:26:38 +0000

    umair aia

--- final.cpp
+++ final.cpp
@@ -19,7 +19,7 @@
 /*2. A train is traveling between 5 cities, and the fare increases with distance. The base fare is $50, 
 and it increases by $20 for each additional city. Display the fare for each city using
  a loop. */
- #include<iostream>
+ /*#include<iostream>
 using namespace std;
 int main(){double a=50;
     for(int i=1; i<=5; i++){
@@ -27,5 +27,46 @@ int main(){double a=50;
 cout<<i<<" City fare is "<<a<<endl;
        a+=20;
     }
+}*/
+
+
+
+
+
+
+
+
+/*3. A clock manufacturer tests clocks for accuracy. A clock runs for 24 hours, and every hour, 
+the error (in seconds) is recorded. The error follows a pattern: 1st hour = 1s, 2nd hour = 2s, 
+3rd hour = 3s, etc. Calculate and display the total time error in seconds after 24 hours. */
+/*#include<iostream>
+using namespace std;
+int main(){
+    float sum;
+for(int i=1; i<=24; i++){
+cout<<i<<" Hour in "<<i<<" second "<<endl;
+sum+=i; 
 }
-                
+cout<<"Total time error in seconds are "<<sum<<" and in  mintues "<<sum/60;
+}*/
+
+      
+
+
+/*6. A farmer is monitoring the growth of a plant for 10 days. Each day, the plant grows by 2 cm more
+ than the previous day (Day 1 = 2 cm, Day 2 = 4 cm, Day 3 = 6 cm, etc.). Use a loop to display the height 
+ of the plant at the end of each day and the total growth after 10 days.*/
+ /*#include<iostream>
+ using namespace std;
+ int main(){
+    int b=2;
+   int sum;
+   sum+=b;
+    for(int i=1; i<=10; i++){
+        cout<<i<<" day plant growth is "<<b<<" cm"<<endl;
+        b+2;
+    }cout<<"Total growth of plant is in 10 days of "<<sum/100<<" meter";
+ }*/
+
+
+ 
