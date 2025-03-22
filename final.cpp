13a60252873c7c25de9ed1056b8b0d4a02af2d78
Umar.dev <01.dev.umar@gmail.com>
2025-03-22 05:53:11 +0000

    chalo

--- final.cpp
+++ final.cpp
@@ -1,6 +1,6 @@
 /*1. A bakery wants to bake 50 loaves of bread each day. Each loaf requires 500 grams of flour. Using a loop,
  calculate and display how much flour is needed for each loaf (1 to 50) and the total required flour. */
- #include<iostream>
+ /*#include<iostream>
  using namespace std;
  int main(){double sum;
  for(int i=1; i<=50; i++){
@@ -10,5 +10,22 @@
      //i++;
  }
  cout<<"Total required flour is "<<sum/1000<<" kilo grams";
- }
+ }*/
  
+
+
+
+ 
+/*2. A train is traveling between 5 cities, and the fare increases with distance. The base fare is $50, 
+and it increases by $20 for each additional city. Display the fare for each city using
+ a loop. */
+ #include<iostream>
+using namespace std;
+int main(){double a=50;
+    for(int i=1; i<=5; i++){
+     //  for(int a=50; a>i; a++){
+cout<<i<<" City fare is "<<a<<endl;
+       a+=20;
+    }
+}
+                
