a204a6ff44e881d52f1076a7ad94cacef74dcedd
Umar.dev <01.dev.umar@gmail.com>
2025-03-22 15:43:29 +0000

    7

--- final.cpp
+++ final.cpp
@@ -53,6 +53,29 @@ cout<<"Total time error in seconds are "<<sum<<" and in  mintues "<<sum/60;
       
 
 
+
+
+/*4. A gym offers discounts based on workout sessions attended in a month (30 days). Every 5th day,
+ a customer gets a 10% discount on their next session fee. Using a loop, display which days a discount
+  will be applied. */
+  /*#include<iostream>
+using namespace std;
+int main(){
+    cout<<"Total fee is 200 per day "<<endl;
+   int total=200;
+    for(int i=5; i<=30; i++){
+      
+     float fill;
+     fill=total*0.1;
+cout<<"Your "<<i<<" day fee is "<<total-fill<<endl;
+ i+=4;
+}
+}*/
+
+
+
+
+
 /*6. A farmer is monitoring the growth of a plant for 10 days. Each day, the plant grows by 2 cm more
  than the previous day (Day 1 = 2 cm, Day 2 = 4 cm, Day 3 = 6 cm, etc.). Use a loop to display the height 
  of the plant at the end of each day and the total growth after 10 days.*/
@@ -70,3 +93,20 @@ cout<<"Total time error in seconds are "<<sum<<" and in  mintues "<<sum/60;
 
 
  
+
+ /*7. A stadium has 100 rows of seats, and each row has 5 more seats than the previous row. 
+The first row has 20 seats. Use a loop to display the number of seats in each row and the total seat
+ in the stadium*/
+ #include<iostream>
+ using namespace std;
+ int main(){
+   double sum;int b=20;
+    for(int i=1; i<=100; i++){
+     
+     cout<<i<<" row in seats "<<b<<endl;
+     sum+=b;
+     b+=5;
+    }  cout<<"Total seats are in stadium "<<sum;
+ }
+ 
+
